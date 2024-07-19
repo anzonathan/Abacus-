39c61761a672f3f8cb88589e9cee2629581f3014
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2024-07-19 21:45:03 +0300

    Create ver0.1.c

---
+++ ver0.1.c
@@ -0,0 +1,38 @@
+//Author: Nathaniel Mugenyi
+//Date: 19, July, 2024
+
+//Description:
+//1. Convert decimal to hex
+//2. Convert hex to decimal
+
+
+#include <stdio.h>
+
+
+int main() {
+
+    int ans;// answer
+
+    while(ans != 0){
+
+
+        int conv; //converting variable
+
+        printf("To convert HEX to DECIMAL enter '1'\nTo convert DECIMAL to HEX enter '2'\nTo exit enter '0'");
+        scanf("%d", ans);
+
+        if(ans == 1){ //convert hex to dec
+
+
+
+
+
+        }
+
+        else if (ans == 2){ //convert dec to hex
+
+
+        }
+    }
+
+}
